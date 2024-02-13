ac03a0d7c2ec530783d9c01cedc923c02fc88b8d
phinsso <naso01247@gmail.com>
2024-02-13 13:55:02 +0900

    코드 리팩토링 문제 풀이 (p.150~151)

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
     @Autowired
@@ -20,15 +22,18 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(MemberForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+        // System.out.println(form.toString());
 
         // 1. dto -> entity
         Member member = form.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
+        // System.out.println(member.toString());
 
         // 2. repository로 entity를 db에 저장
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        // System.out.println(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/firstproject/dto/MemberForm.java
+++ src/main/java/com/example/firstproject/dto/MemberForm.java
@@ -1,23 +1,14 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class MemberForm {
     private String email; // 이메일을 받을 필드
     private String password; // 비밀번호를 받을 필드
-
-    public MemberForm(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberForm{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 
     public Member toEntity() {
         return new Member(null, email, password);
