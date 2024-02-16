3ec4c3338993a211ebca66a6ebef12aa39f93ec9
phinsso <naso01247@gmail.com>
2024-02-16 12:31:11 +0900

    데이터 조회 (read) 문제 풀이 (p.182)

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Slf4j
 @Controller
 public class MemberController {
     @Autowired
-    private MemberRepository memberRepository;
+    MemberRepository memberRepository;
 
     @GetMapping("/signup")
     public String signUpPage() {
@@ -25,18 +27,32 @@ public class MemberController {
     @PostMapping("/join")
     public String join(MemberForm form) {
         log.info(form.toString());
-        // System.out.println(form.toString());
 
         // 1. dto -> entity
         Member member = form.toEntity();
         log.info(member.toString());
-        // System.out.println(member.toString());
 
         // 2. repository로 entity를 db에 저장
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
-        // System.out.println(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/members")
+    public String show(@PathVariable Long id, Model model) {
+        // 1. id 조회해 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        // 3. 뷰 페이지 반환하기
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        List<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("member", memberEntityList);
+        return "members/index";
     }
 }
