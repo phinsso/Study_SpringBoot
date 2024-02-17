d46e6253b530cf96232c5c7a9f6043cff221e40d
phinsso <naso01247@gmail.com>
2024-02-17 14:15:02 +0900

    댓글 엔티티 리파지터리 생성, 테스트 코드 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,32 @@
+-- article
 INSERT INTO article(title, content) VALUES('가가가가', '1111');
 INSERT INTO article(title, content) VALUES('나나나나', '2222');
 INSERT INTO article(title, content) VALUES('다다다다', '3333');
 
+-- coffee
 INSERT INTO coffee(name, price) VALUES('아메리카노', '4500');
 INSERT INTO coffee(name, price) VALUES('라떼', '5000');
 INSERT INTO coffee(name, price) VALUES('카페모카', '5500');
+
+-- article(댓글용 게시물)
+INSERT INTO article(title, content) VALUES('당신의 인생 영화는?', '댓글 고');
+INSERT INTO article(title, content) VALUES('당신의 소울 푸드는?', '댓글 고고');
+INSERT INTO article(title, content) VALUES('당신의 취미는?', '댓글 고고고');
+
+-- comment(4번 게시글)
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Park', '굿 윌 헌팅');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Kim', '아이 엠 샘');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Choi', '쇼생크 탈출');
+
+-- comment(5번 게시글)
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Park', '치킨');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Kim', '피자');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Choi', '초밥');
+
+-- comment(6번 게시글)
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Park', '조깅');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Kim', '유튜브 시청');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Choi', '독서');
+
+
 
